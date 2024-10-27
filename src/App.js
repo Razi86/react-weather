e18e8e7ffeb565c73f02e96e73f320c847b10561
Razi86 <112767544+Razi86@users.noreply.github.com>
2024-10-27 19:00:47 +0100

    add github link to project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
       <Search />
+      <p>
+      This project was coded by Razi and is <a href="https://github.com/Razi86/react-weather" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      </p>
       </header>
     </div>
   );
